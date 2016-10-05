7d1d2f1d400d889dc7609b7aa09ad5e98466ac58
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2016-10-05 17:12:42 -0300

    Refactoring

--- bucky/index.php
+++ bucky/index.php
@@ -1,62 +1,55 @@
 
 <?php 
-// https://phpbestpractices.org/
-// http://br.phptherightway.com/
-// Most common mistakes;
-// http://www.w3schools.com/php/php_form_validation.asp
-	require ("helper/ServerHelper.php");
 	require ("helper/Helper.php");
-?>
-
+	require ("helper/ServerHelper.php"); ?>
 
 <!DOCTYPE html>
 <html>
 <body>
 
-<form method="post" action="<?php echo htmlspecialchars(getFromServer('PHP_SELF'));?>">
-  Name: <input type="text" name="fname">
-  <input type="submit">
-</form>
+	<?php include ("header.php"); ?>
 
-<?php 
-	include ("header.php");// for menus
- 	
+	<form 
+		method="post" 
+		action="<?php echo htmlspecialchars(getFromServer('PHP_SELF'));?>" >
+		
+	  	Name: <input type="text" name="fname">
+	  	<input type="submit">
+	  	
+	</form>
 
-	showServerInfo();
-
-	function showServerInfo() {
-		echo ServerHelper::getPhpSelf();
-		echo "<br>";
-		echo $_SERVER['SERVER_NAME'];
-		echo "<br>";
-		echo $_SERVER['HTTP_HOST'];
-		echo "<br>";
-		echo $_SERVER['HTTP_USER_AGENT'];
-		echo "<br>";
-		echo $_SERVER['SCRIPT_NAME'];
-	}
+	<?php 
+		showServerInfo();
 	
-	if (isRequestPost()) {
-		
-		$name = requestName();
-		
-		if (isEmpty($name)) {
-			echo "Name is empty";
-		} else {
-			echo $name;
+		function showServerInfo() {
+			echo ServerHelper::getPhpSelf();
+			echo "<br>";
+			echo $_SERVER['SERVER_NAME'];
+			echo "<br>";
+			echo $_SERVER['HTTP_HOST'];
+			echo "<br>";
+			echo $_SERVER['HTTP_USER_AGENT'];
+			echo "<br>";
+			echo $_SERVER['SCRIPT_NAME'];
 		}
-	}
-	
-	function requestName() {
-		return $_REQUEST['fname'];
-	}
-	
-	
-?>
-
+		
+		if (isRequestPost()) {
+			
+			$name = requestName();
+			
+			if (isEmpty($name)) {
+				echo "Name is empty";
+			} else {
+				echo $name;
+			}
+		}
+		
+		function requestName() {
+			return $_REQUEST['fname'];
+		}
+		
+		
+	?>
 
 </body>
 </html>
-
-
-
